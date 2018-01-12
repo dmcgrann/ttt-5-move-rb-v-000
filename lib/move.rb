9202d440bb952002d8730b838da660e43a0489cd
David McGrann <dpmcgrann@gmail.com>
2018-01-12 00:41:55 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,7 @@ end
 def input_to_index(user_input)
   user_input = "1...9"
   converted_input = user_input.to_i
+  converted_input -= 1
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
